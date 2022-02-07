3d8fb5187a1b6c6b40b571dcaef0419a1f8eec1d
ffzydxl <475315876@qq.com>
2022-02-07 20:49:42 +0800

    second commit

---
+++ Demo02.java
@@ -0,0 +1,14 @@
+public class Demo02 {
+    public static void main(String[] args){
+        char a = 'a';
+        char b = 'A';
+        //boolean flag = true;
+        //System.out.println(a=b);
+        System.out.println((int)a);
+        System.out.println((int)b);
+    }
+}
+
+
+
+
