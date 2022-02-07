f77d1df1d77cd0260c9aaa0f627bfb73c4eec5fb
ffzydxl <475315876@qq.com>
2022-02-07 20:14:28 +0800

    this is my first file

---
+++ Hello.java
@@ -0,0 +1,18 @@
+public class Hello {
+    public static void main(String[] args){
+        String name01 = "倔强王者";
+        String name02 = "荣耀青铜";
+        System.out.println(name01);
+        System.out.println(name02);
+    }
+//我是pcz
+   /*
+   我是pcz
+   我是pcz
+    */
+
+    /**
+     * @Descriptiom Hello Word
+     * @Descriptiom Hello Word
+     */
+}//////////////////
